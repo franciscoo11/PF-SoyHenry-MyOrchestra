c3fab4ed17c529d031fb4fcfd7a0b2807286c069
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-07 14:54:11 -0300

    integrate auth0 and envolve app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { UserProvider } from '@auth0/nextjs-auth0'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
