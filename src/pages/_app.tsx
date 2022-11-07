b67c246eb4c5d06eadaedcfb2858f4404931c181
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-07 15:07:34 -0300

    update and perform integration with provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
 
--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,9 +1,3 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth } from "@auth0/nextjs-auth0"
 
-export default handleAuth({
-  async login(req, res) {
-    await handleLogin(req, res, {
-      returnTo: "/",
-    });
-  },
-});
+export default handleAuth()
