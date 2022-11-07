af622d82a70a6948badc8138e0b963554b99612f
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-11-07 23:02:17 +0000

    utils/fakeDB/updated

--- src/frontend/utils/fakeDB.tsx
+++ src/frontend/utils/fakeDB.tsx
@@ -1,4 +1,4 @@
-const Orquestas = [
+export const Orquestas = [
     {
     name: "Orquesta 1",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga adipisci qui, quae numquam amet voluptate aspernatur vel libero! Eius, velit.",
@@ -9,7 +9,168 @@ const Orquestas = [
     orchestra_type: "Sinfónica",
     donations_acount: "Terrell.Emmerich98@hotmail.com",
     phone: "535-808-3320"
+},
+{
+    name: "Orquesta 2",
+    description: "Nesciunt laboriosam vitae aut beatae porro consequatur qui vitae quos. Explicabo reprehenderit aut qui aliquid. Ratione asperiores laudantium quod ea. Ab et quis a tempore ut sed. In sint non rem quae harum est at et.",
+    logo: "https://static8.depositphotos.com/1378583/986/v/600/depositphotos_9866608-stock-illustration-orchestra-logo.jpg",
+    cover: "https://cdn.pixabay.com/photo/2018/01/18/12/39/music-3090204_960_720.jpg",
+    sponsors: ["Patrocinador 1", "Patrocinador 2", "Patrocinador 3"],
+    ubication: "West Virginia, Oman",
+    orchestra_type: "Filarmónica",
+    donations_acount: "Paxton10@yahoo.com",
+    phone: "870-268-9274"
+},
+{
+    name: "Orquesta 3",
+    description: "Eius nisi repellendus. Fugiat aut similique et fuga cumque qui exercitationem nesciunt. Numquam minima aperiam at harum id quis omnis.",
+    logo: "https://img.freepik.com/vector-premium/logotipo-instrumento-musical-violin-piano-grupo-orquesta_468322-227.jpg",
+    cover: "https://cdn.pixabay.com/photo/2020/12/09/18/42/violin-5818267_960_720.jpg",
+    sponsors: ["Patrocinador 1", "Patrocinador 2", "Patrocinador 3"],
+    ubication: "Maryland, Haiti",
+    orchestra_type: "Orquesta de Cámara",
+    donations_acount: "Jamil_Roob88@yahoo.com",
+    phone: "717-344-3080"
+},
+{
+    name: "Orquesta 4",
+    description: "Error nam consequatur pariatur similique temporibus. Atque provident corporis laborum in voluptatem et. Dolores quae perspiciatis illo. Corporis voluptatem placeat. Occaecati optio perferendis deserunt laborum rerum. Accusamus quo et sit praesentium accusamus ut illo sint qui.",
+    logo: "https://pbs.twimg.com/profile_images/692948683344707585/kAF0LdgQ_400x400.jpg",
+    cover: "https://cdn.pixabay.com/photo/2020/05/11/09/03/conductor-5157153_960_720.jpg",
+    sponsors: ["Patrocinador 1", "Patrocinador 2", "Patrocinador 3"],
+    ubication: "Pennsylvania, Paraguay",
+    orchestra_type: "Orquesta de Cuerdas",
+    donations_acount: "Meta.Cole@gmail.com",
+    phone: "626-918-6270"
 }
-]
 
-export default Orquestas;
+];
+
+export const Users = [
+    {
+        name: "Kellie Auer",
+        email: "River.Koch@example.net",
+        password: "wmAosoOrBGF2uKg",
+        image: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/614.jpg",
+        birthday: "Fri Apr 08 2022 12:07:36 GMT+0000 (Coordinated Universal Time)",
+        city: "Port Wilmahaven",
+        rol: "Teacher"
+    },
+    {
+        name: "Beverly Buckridge II",
+        email: "Dale_Doyle18@gmail.com",
+        password: "jRbVqZDJlewevyQ",
+        image: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/37.jpg",
+        birthday: "Sat Dec 25 2021 15:36:17 GMT+0000 (Coordinated Universal Time)",
+        city: "Tinley Park",
+        rol: "Director"
+    },
+    {
+        name: "Latoya Klein",
+        email: "Don8@hotmail.com",
+        password: "hGOziQSu67eSoHQ",
+        image: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/168.jpg",
+        birthday: "Mon Jan 03 2022 01:24:08 GMT+0000 (Coordinated Universal Time)",
+        city: "East Romainetown",
+        rol: "Student"
+    },
+    {
+        name: "Cody Roberts",
+        email: "Nicola_Franecki@gmail.com",
+        password: "2sJ_KRysfWFszpW",
+        image: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/465.jpg",
+        birthday: "Fri Dec 17 2021 12:24:29 GMT+0000 (Coordinated Universal Time)",
+        city: "Remingtonport",
+        rol: "Student"
+    }
+];
+
+export const Posts = [
+    {
+        title: "Eos magni ipsum.",
+        content: "Blanditiis harum quidem tenetur voluptatem. Non dolorum vel repellat recusandae sunt ut. Enim deserunt et dolor et eveniet ipsam sed. Iste in ducimus consequatur id quis soluta. Excepturi tenetur quo dolor. Illum ut eum.",
+        media: "http://placeimg.com/640/480/business",
+        views: 91625,
+        event_date: "Sat Dec 03 2022 20:50:41 GMT+0000 (Coordinated Universal Time)",
+        file: "card_keyboard_enterprise.pdf"
+    },
+    {
+        title: "Laborum cum ea aliquid.",
+        content: "Repudiandae illo accusantium sequi illum amet suscipit quia est voluptates. Non pariatur possimus in sapiente facilis. Labore ipsam qui dolore autem. Iste neque nihil dolorem non nisi quam est. Nisi rem quod. Placeat hic occaecati.",
+        media: "http://placeimg.com/640/480/nature",
+        views: 19671,
+        event_date: "Sun Oct 01 2023 17:45:27 GMT+0000 (Coordinated Universal Time)",
+        file: "generic.mp4"
+    },
+    {
+        title: "Unde beatae nostrum accusantium voluptatem aspernatur.",
+        content: "Error est dignissimos soluta facere nihil inventore molestiae rem. At soluta unde veniam ut iure doloremque temporibus beatae dolorem. Quasi perferendis veritatis velit asperiores. Eaque molestiae quo voluptates provident accusamus numquam voluptate.",
+        media: "http://placeimg.com/640/480/cats",
+        views: 24075,
+        event_date: "Mon Nov 21 2022 11:34:25 GMT+0000 (Coordinated Universal Time)",
+        file: "malawi_incredible.csv"
+    },
+    {
+        title: "Dolor iusto numquam perferendis cupiditate et aut cupiditate ratione deserunt.",
+        content: "Quae suscipit in voluptas distinctio molestiae cumque dolore. Est dolores autem quisquam doloribus molestiae. Hic tenetur et dolore quia. Aliquam eos minima corrupti labore vel sit ea praesentium nemo. Doloremque odio consequatur libero ipsam sit accusamus atque voluptatem ut.",
+        media: "http://placeimg.com/640/480/fashion",
+        views: 79025,
+        event_date: "Mon Sep 25 2023 00:56:02 GMT+0000 (Coordinated Universal Time)",
+        file: "com.wav"
+    }
+
+];
+
+export const Comments = [
+    {
+        user: "Sydnee.Torphy",
+        content: "Aut eveniet ut ut enim et aspernatur consequatur natus. Molestiae est libero suscipit. Quae sed debitis amet iusto numquam atque."
+    },
+    {
+        user: "Mariana.Veum20",
+        content: "Inventore dolor omnis exercitationem dolores ratione. Tempore quod perferendis sed reiciendis fugit autem. Assumenda ducimus odit et. Praesentium occaecati asperiores corporis. Blanditiis quis natus commodi."
+    },
+    {
+        user: "General76",
+        content: "Optio ut ut quas ut nihil quo vel. Aut dolorum a amet perferendis aperiam nam enim tenetur quia. Est cupiditate dolores rerum voluptates atque ab dicta. Deserunt sapiente et laborum commodi natus aut dolor fugit eius. Ut aliquam qui beatae. Consequatur voluptatem dolor distinctio."
+    },
+    {
+        user: "Rowland26",
+        content: "Sint est exercitationem dolorem consequuntur occaecati sunt provident nobis numquam. Alias et nostrum facere soluta numquam quidem corrupti beatae. Rerum porro velit quibusdam nemo id at et ipsam. Ullam id suscipit ut et id quasi deleniti maxime."
+    }
+];
+
+export const Campaigns = [
+    {
+        title: "Impedit ea ea.",
+        description: "Quasi molestias autem incidunt at ratione aut explicabo enim quia. Sapiente cupiditate aut et. Id veniam autem dolor omnis. Sit sunt reprehenderit sed.",
+        goal_amount: 237.98,
+        start_date: "Sun Oct 09 2022 15:30:02 GMT+0000 (Coordinated Universal Time)",
+        end_date: "Thu Jul 13 2023 02:34:32 GMT+0000 (Coordinated Universal Time)",
+        amount_raised: 642.17,
+    },
+    {
+        title: "Excepturi soluta magni mollitia non nulla deserunt.",
+        description: "Ad architecto odit voluptatem et beatae ullam ratione error dolor. Odit voluptas commodi voluptas occaecati. Dignissimos tempore occaecati dolor rerum. Dolore et esse aut non et dolores consequatur. Quis officiis minima deserunt qui sint voluptas vero.",
+        goal_amount: 382.55,
+        start_date: "Tue Feb 08 2022 07:43:47 GMT+0000 (Coordinated Universal Time)",
+        end_date: "Fri Jun 16 2023 17:32:31 GMT+0000 (Coordinated Universal Time)",
+        amount_raised: 741.78,
+    },
+    {
+        title: "Accusantium harum iste consequatur suscipit harum officia minima aut.",
+        description: "Veniam assumenda debitis. Quis perspiciatis ea. Modi recusandae quaerat quis quo eum consectetur est ut commodi. Sit et incidunt libero ut ipsam architecto eligendi eum. Est id sed assumenda est. Corrupti ducimus sequi possimus quia alias tempora aliquam nemo sapiente.",
+        goal_amount: 217.28,
+        start_date: "Tue Jul 19 2022 16:02:23 GMT+0000 (Coordinated Universal Time)",
+        end_date: "Thu Jan 12 2023 16:17:05 GMT+0000 (Coordinated Universal Time)",
+        amount_raised: 740.12,
+    },
+    {
+        title: "Nostrum ut unde explicabo quia.",
+        description: "Itaque aperiam non. Sed dolor harum. Aspernatur reiciendis iste occaecati doloribus fugiat facilis dolorem laudantium. Repudiandae est ut non. Quo quam distinctio voluptas optio ea consequuntur dolores et aut. At recusandae itaque sit est dicta et ullam.",
+        goal_amount: 910.59,
+        start_date: "Mon Mar 28 2022 17:45:13 GMT+0000 (Coordinated Universal Time)",
+        end_date: "Thu Apr 20 2023 23:53:34 GMT+0000 (Coordinated Universal Time)",
+        amount_raised: 500.08,
+    }
+]
