59b53234b3a51e1b34d7c6204c519b2514b927f5
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-07 14:56:18 -0300

    fix some error with provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { UserProvider } from '@auth0/nextjs-auth0'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
