629cb9bc1a94dff1dc176fb596e72da2d11d3a2d
Jartisick <Jartisick@gmail.com>
2022-11-08 17:49:35 -0400

    componente SearchBar para la barra de navegación

---
+++ src/frontend/components/SearchBar.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const searchlogo =
+  "https://res.cloudinary.com/dzup1ckpy/image/upload/v1667940299/clipart4618545_kd7rwh.png";
+
+const SearchStyles = styled.div`
+  form {
+    border-radius: 25px;
+    background-color: #f7f5f5;
+    padding: 10px;
+  }
+  input {
+    background: transparent;
+    flex: 1;
+    border: 0;
+    outline: none;
+  }
+  button {
+    background: transparent;
+    border: 0;
+    cursor: pointer;
+  }
+`;
+const SearchBar = () => {
+  return (
+    <SearchStyles>
+      <form>
+        <button>
+          <img src={searchlogo} width="15px" height="12,5px" />
+        </button>
+        <input type="text" placeholder="Buscar" />
+      </form>
+    </SearchStyles>
+  );
+};
+
+export default SearchBar;
