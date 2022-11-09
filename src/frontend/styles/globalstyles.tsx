412786ad71c3a7cfaa276e54716cb7a4a321a249
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-11-09 06:43:03 +0000

    front/fix-font-import

--- src/frontend/styles/globalstyles.tsx
+++ src/frontend/styles/globalstyles.tsx
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
   html,
   body {
     color: ${({ theme }) => theme.colors.primary};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
+import Head from "next/head";
 import HeroImage from "../frontend/components/HeroImage";
 import HomeMainContent from "../frontend/components/HomeMainContent";
 import MainNavBar from "../frontend/components/MainNavBar";
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Red de Orquestas Populares de Música Latinoamericana</title>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
+        </style>
+      </Head>
       <MainNavBar />
       <HeroImage />
       <HomeMainContent />
-    </div>
+    </>
   );
 }
