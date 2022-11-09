696e17b3f7e1bf4ab13573ad1c9a965580951330
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-09 16:03:15 -0300

    changes controllers and logic inside them

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,32 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 // import { resourceUsage } from 'process';
+import { getUsers, postUser, updateUser, deleteUser, logicDeleteUser } from '../../../controllers/user';
 
 
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
     const GET:string="GET";
     const POST:string="POST";
     const DELETE:string="DELETE";
-
+    const PUT:string="PUT";
 
     let {
       method,
-      body: { papas,patatas },
-      query: {  },
+      body,
+      query,
     } = req;
 
 
     try {
         switch (method) {
             case GET:
-            return res.status(200).json( {name: 'SOY GET'})
+              const response = await getUsers()
+            return res.status(201).json(response)
             case POST:
-            return res.status(200).json({ name: 'SOY POST' })
+              const response2 = await postUser(body)
+            return res.status(200).json(response2)
+            case PUT:
+              const response3 = await updateUser(query.id,body)
+              console.log(response3)
+            return res.status(200).json(response3)
             case DELETE:
-            return res.status(200).json({ name: 'SOY DELETE' })
+              const response4 = await deleteUser(query.id)
+              console.log(response4)
+              return res.status(200).json(response4)
             default:
             return res.status(400).json("method no found")
               
