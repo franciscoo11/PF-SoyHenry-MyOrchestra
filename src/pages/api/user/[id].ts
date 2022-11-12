cb31df34986265bd6c1bb6f130042fad5d2c9854
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-12 15:43:50 -0300

    fix handler error and format response

--- src/pages/api/user/[id].ts
+++ src/pages/api/user/[id].ts
@@ -23,13 +23,13 @@ export default async function handler(
         switch (method) {
             case GET:
               const getUser = await getUsers(id)
-            return getUser ? res.status(200).json(getUser) : res.status(404).json({error: 'User not found'})
+            return getUser ? res.status(200).json(getUser) : res.status(404).json({error: 'User not found, check if id is valid'})
             case PATCH:
               const fakeDeleteUser = await logicDeleteUser(id)
-            return fakeDeleteUser ? res.status(204).json(fakeDeleteUser) : res.status(404).json({error: 'Something goes wrong, check id and try again'})
+            return fakeDeleteUser ? res.status(204).json(fakeDeleteUser) : res.status(404).json({error: 'Something goes wrong, check if id is correct and try again'})
             case PUT:
               const modifyUser = await updateUser(id,body)
-            return modifyUser ? res.status(204).json(modifyUser) : res.status(404).json({error: 'Something goes wrong, check information sended and try again'})
+            return modifyUser ? res.status(204).json(modifyUser) : res.status(404).json({error: 'Something goes wrong, check if id is correct and try again'})
             case DELETE:
               const removeUser = await deleteUser(id)
               return removeUser ? res.status(200).json(removeUser) : res.status(404).json({error: 'Something goes wrong, check id and try again'})
