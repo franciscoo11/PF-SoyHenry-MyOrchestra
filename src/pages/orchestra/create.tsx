0e49d86d009f2803076ff23fbcc002b215896cf7
Jartisick <Jartisick@gmail.com>
2022-11-14 12:23:28 -0400

    cambios

--- src/pages/orchestra/create.tsx
+++ src/pages/orchestra/create.tsx
@@ -293,10 +293,20 @@ export default function CreateOrchestra(props: Props) {
             <div className="orchesta-type-field">
               <Field
                 name="orchestra_TypeId"
-                type="text"
+                as="select"
                 placeholder="Tipo de Orquesta"
                 className="input"
-              />
+              >
+                <option disabled value="">
+                  Tipo de Orquesta
+                </option>
+                {props.types_orchestras &&
+                  props.types_orchestras.map((type_orq: any) => (
+                    <option value={type_orq.id} key={type_orq.id}>
+                      {type_orq.type}
+                    </option>
+                  ))}
+              </Field>
               <p className="error">
                 <ErrorMessage
                   name="orchestra_TypeId"
