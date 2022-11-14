b9c3b0d70a34485d143bf4feb9116e142ca059c0
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-14 19:11:03 -0300

    hotfix/fix bug in orchestras

--- src/controllers/orchestras.ts
+++ src/controllers/orchestras.ts
@@ -36,15 +36,7 @@ export const getOrchestras = async (query: any) => {
   }
 
 
-  const orchestras = await prisma.orchestra.findMany({
-    include:{
-      posts:{
-        include:{
-          PostUserOnReaction:true
-        }
-      }
-    }
-  });
+  const orchestras = await prisma.orchestra.findMany();
   if (location || orchestra_TypeId) return filter_query(orchestras, location, orchestra_TypeId)
   return orchestras ? orchestras : undefined;
 };
