bfc8e0c0f8ea20ed4b2839f570769a73d1bbd904
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-16 20:21:56 -0300

    hotfix/check-rol-working

--- src/pages/api/rols/[id].ts
+++ src/pages/api/rols/[id].ts
@@ -1,27 +1,27 @@
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { updateRol } from "../../../controllers/rols";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { updateRol } from "../../../controllers/rols";
 
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
 
-//   const PUT: string = "PUT";
-//   let { method, body, query } = req;
+  const PUT: string = "PUT";
+  let { method, body, query } = req;
 
-//   try {
-//     switch (method) {
-//       case PUT:
-//         const changeRol = await updateRol(query.id,body.name);
-//         return changeRol
-//           ? res.status(200).json(changeRol)
-//           : res.status(404).json({ error: "Something goes wrong, try again or check id" });
-//       default:
-//         return res.status(400).json("method not allowed");
-//     }
-//   } catch (error) {
-//     return res
-//       .status(400)
-//       .json({ error: "Internal error, something goes really really wrong" });
-//   }
-// }
+  try {
+    switch (method) {
+      case PUT:
+        const changeRol = await updateRol(query.id,body.rolId);
+        return changeRol
+          ? res.status(200).json(changeRol)
+          : res.status(404).json({ error: "Something goes wrong, try again or check id" });
+      default:
+        return res.status(400).json("method not allowed");
+    }
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: "Internal error, something goes really really wrong" });
+  }
+}
