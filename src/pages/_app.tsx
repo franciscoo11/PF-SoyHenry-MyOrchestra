8a803546564072b391c6dc44c492ebd07ae7bb15
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-17 19:03:33 -0300

    add session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider, DefaultTheme } from "styled-components";
 import GlobalStyle from "../frontend/styles/globalstyles";
+import { SessionProvider } from 'next-auth/react';
 
 const theme: DefaultTheme = {
   colors: {
@@ -11,11 +12,13 @@ const theme: DefaultTheme = {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <> 
+      <SessionProvider session={ pageProps.session }>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
+      </SessionProvider>
     </>
   );
 }
