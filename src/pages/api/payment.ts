8bf8f3c077fd1fd65bdb8c43c60a6ca587bac2ea
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-11-22 23:58:49 -0300

    fix identation

--- src/pages/api/payment.ts
+++ src/pages/api/payment.ts
@@ -1,11 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import mercadopago from 'mercadopago';
 
-
 mercadopago.configure({
   access_token: process.env.MERCADOPAGO_TOKEN || ''
 });
-
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
  switch (req.method) {
@@ -66,7 +64,6 @@ function searchPay(req: NextApiRequest, res: NextApiResponse<any>) {
   //   begin_date: mercadopago.utils.date.now().subtract(60).toString(),
   //   end_date: mercadopago.utils.date.now().toString()
   // };
-
 
   mercadopago.payment.search({
     qs: filters
