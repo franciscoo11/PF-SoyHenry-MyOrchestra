59aceb557b28fb833281c216eeede477f03e6346
Ivan Garcia <hectorivang2000@gmail.com>
2022-11-24 09:23:54 -0500

    control y camtura de datos usuario

--- src/frontend/components/Auth.tsx
+++ src/frontend/components/Auth.tsx
@@ -3,25 +3,30 @@ import LoggedOut from "./LoggedOut";
 import { useUser } from "@auth0/nextjs-auth0/dist/frontend/use-user";
 import Cookies from "universal-cookie";
 import { useEffect, useState } from "react";
+import {getUserEmail  } from "../utils/getUserEmail";
 
 export default function Auth() {
   const { user, error, isLoading } = useUser();
   const cookie = new Cookies();
-  cookie.set("Userlogin", user, { path: "/" });
+  cookie.set("Userlogin", user, { path: "/" })
   const [loging, setLoging] = useState();
   const { cookies }: any = cookie;
+ 
 
   useEffect(() => {
+    getUserEmail(user)
     setLoging(cookie.get("Userlogin"));
-  }, [cookies.Userlogin]);
+  }, [cookies.Userlogin,user]);
 
+  console.log();
+  
   const handlelogout = () => {
     cookie.remove("Userlogin");
-    setLoging(cookie.get("Userlogin"));
+    cookie.remove("UserloginData");
+    setLoging(cookie.get("Userlogin"));   
   };
-
   if (loging === "undefined" || loging === undefined) {
-    return <LoggedOut />;
+    return <LoggedOut  />;
   } else {
     return <LoggedIn handlelogout={handlelogout} />;
   }
