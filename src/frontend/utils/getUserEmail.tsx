9764d5d1c28b4a98f544c17102fed5e1944874bb
Ivan Garcia <hectorivang2000@gmail.com>
2022-11-24 10:36:26 -0500

    fixerror

--- src/frontend/utils/getUserEmail.tsx
+++ src/frontend/utils/getUserEmail.tsx
@@ -4,7 +4,7 @@ import Cookies from "universal-cookie";
 export const getUserEmail=async (user:any)=>{ 
     const cookie = new Cookies();
     if(user?.email_verified){   
-      await  axios.post(`http://localhost:3000/api/user?isGmail=${user?.email_verified}`,{
+      await  axios.post(`/api/user?isGmail=${user?.email_verified}`,{
         email:user?.email,
         name: user?.name,
         avatar: user?.picture
@@ -16,7 +16,7 @@ export const getUserEmail=async (user:any)=>{
     
 }else if(user?.email){
 
-    await  axios.get(`http://localhost:3000/api/user/${user?.email}`)
+    await  axios.get(`/api/user/${user?.email}`)
     .then(
         (response)=>{
             cookie.set("UserloginData", response.data, { path: "/" })
