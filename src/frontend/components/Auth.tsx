d4b9f6350fbf7500a95b887a922c7d50f07a3a24
hansey <javierjose2555@gmail.com>
2022-11-29 18:33:49 -0500

    we have to repair a little things about ban user

--- src/frontend/components/Auth.tsx
+++ src/frontend/components/Auth.tsx
@@ -3,28 +3,55 @@ import LoggedOut from "./LoggedOut";
 import { useUser } from "@auth0/nextjs-auth0/dist/frontend/use-user";
 import Cookies from "universal-cookie";
 import { useEffect, useState } from "react";
-import { getUserEmail } from "../utils/getUserEmail";
+import {getUserEmail  } from "../utils/getUserEmail";
+import { toast,ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Auth() {
   const { user, error, isLoading } = useUser();
   const cookie = new Cookies();
-  cookie.set("Userlogin", user, { path: "/" });
+  cookie.set("Userlogin", user, { path: "/" })
   const [loging, setLoging] = useState();
   const { cookies }: any = cookie;
+ 
 
   useEffect(() => {
-    getUserEmail(user);
+     getUserEmail(user)
     setLoging(cookie.get("Userlogin"));
-  }, [cookies.Userlogin, user]);
+    console.log(cookie.get("UserloginData"));
+  }, [cookies.Userlogin,user]);
 
+
+  
   const handlelogout = () => {
     cookie.remove("Userlogin");
     cookie.remove("UserloginData");
-    setLoging(cookie.get("Userlogin"));
+    setLoging(cookie.get("Userlogin"));   
   };
   if (loging === "undefined" || loging === undefined) {
-    return <LoggedOut />;
-  } else {
-    return <LoggedIn handlelogout={handlelogout} id={user?.email} />;
+    return <LoggedOut  />;
+  
+  } else if(cookie.get('UserloginData')){
+    if (!cookie.get("UserloginData").is_active) {
+      toast.error("Verifica e intenta nuevamente.", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      cookie.remove("Userlogin");
+      cookie.remove("UserloginData");
+      setLoging(cookie.get("Userlogin"));  
+      window.location.href='/bannedPage';
+      <ToastContainer />
+       
+    }else{
+      return <LoggedIn handlelogout={handlelogout} />;
+    }
   }
 }
+
