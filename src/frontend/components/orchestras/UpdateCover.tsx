bb8e987aa08867a9c47a9da30254f8ebb4e50b41
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-11-29 05:09:06 +0000

    reactions fix & cover img btn styled

--- src/frontend/components/orchestras/UpdateCover.tsx
+++ src/frontend/components/orchestras/UpdateCover.tsx
@@ -4,7 +4,56 @@ import { useRouter } from "next/router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { FiCamera } from "react-icons/fi";
+import styled from "styled-components";
 
+const StyledDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  /**********File Inputs**********/
+  .container-input {
+    position: absolute;
+    bottom: 12px;
+    right: 12px;
+  }
+
+  .inputfile {
+    width: 0.1px;
+    height: 0.1px;
+    opacity: 0;
+    overflow: hidden;
+    position: absolute;
+    z-index: -1;
+  }
+
+  .inputfile + label {
+    font-size: 0.8em;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    cursor: pointer;
+    display: inline-block;
+    padding: 10px;
+  }
+
+  .inputfile + label svg {
+    vertical-align: middle;
+  }
+
+  /* style  */
+
+  .inputfile-2 + label {
+    color: white;
+    border: 1px solid currentColor;
+    border-radius: 10px;
+  }
+
+  .inputfile-2:focus + label,
+  .inputfile-2.has-focus + label,
+  .inputfile-2 + label:hover {
+    color: lightgray;
+  }
+`;
 export const UpdateCover = ({ id }: any) => {
   const router = useRouter();
 
@@ -46,72 +95,21 @@ export const UpdateCover = ({ id }: any) => {
   }
 
   return (
-    <>
-      {/* <Formik
-        initialValues={{
-          cover: "",
-        }}
-        onSubmit={async (values: any, { setSubmitting }) => {
-          const file: any = values.file;
-          const formData = new FormData();
-          try {
-            formData.append("file", file);
-            formData.append("upload_preset", "orchestras-uploads");
-            const uploadImage: any = await axios.post(
-              `https://api.cloudinary.com/v1_1/orchestrascloudinary/image/upload`,
-              formData
-            );
-            console.log(uploadImage);
-
-            let postData = {
-              cover: uploadImage.data.secure_url,
-            };
-            await axios.put(`/api/orchestra/${id}`, postData);
-            console.log(postData);
-            toast.success("Cover actualizado correctamente", {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              theme: "light",
-            });
-            router.push(`/orchestras/${id}`);
-            setSubmitting(false);
-          } catch (error) {}
-        }}
-      >
-        {({ setFieldValue }) => (
-          <Form>
-            <Field
-              type="file"
-              name="cover"
-              onChange={(event: any) => {
-                setFieldValue("file", event.target.files[0]);
-              }}
-            />
-            <button type="submit">Actualizar</button>
-          </Form>
-        )}
-      </Formik>
-      <ToastContainer /> */}
+    <StyledDiv>
       <div className="container-input">
         <input
-          className="inputfile inputfile-5"
+          className="inputfile inputfile-2"
           type="file"
           name="file"
           id="file"
           onChange={handleImageLoad}
         />
         <label htmlFor="file">
-          <figure>
-            <FiCamera className="iborrainputfile" />
-          </figure>
+          <span className="iborrainputfile">Cambiar </span>
+          <FiCamera className="iborrainputfile" />
         </label>
       </div>
       <ToastContainer />
-    </>
+    </StyledDiv>
   );
 };
