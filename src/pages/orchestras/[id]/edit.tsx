f410a11689b4c3e4d06778d835bfacd43321b1dd
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-11-30 23:20:54 +0000

    fix edit form orchestra

--- src/pages/orchestras/[id]/edit.tsx
+++ src/pages/orchestras/[id]/edit.tsx
@@ -9,50 +9,45 @@ import { StyledMain } from "../../../frontend/styles/orchestras/sharedStyles";
 import { prisma } from "../../../../lib/prisma";
 import CampaignForm from "../../../frontend/components/orchestras/CampaignForm";
 import OrchestraEditForm from "../../../frontend/components/OrchestraEditForm";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export interface DataModel {
   id: string;
 }
 
-export const getStaticPaths = async () => {
+export async function getServerSideProps({ params }: any) {
   try {
-    const orchestrasById: any =
-      await prisma.$queryRaw`SELECT id FROM orchestras`;
-    const paths = orchestrasById.map(({ id }: any) => ({ params: { id } }));
-    return {
-      paths,
-      fallback: false,
-    };
+    const orchestra = await prisma.orchestras.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+
+    const members = await prisma.users_on_orchestra.findMany({
+      where: {
+        orchestraId: params.id,
+      },
+      include: {
+        user: true,
+        rol: true,
+      },
+    });
+
+    const types_orchestras = await prisma.orchestra_type.findMany();
+
+    return { props: { orchestra, members, types_orchestras } };
   } catch (error) {
     console.log(error);
   }
-};
-
-export const getStaticProps = async ({ params }: any) => {
-  try {
-    const orchestraQuery: any =
-      await prisma.$queryRaw`SELECT * FROM orchestras WHERE id = ${params.id}`;
-    const types_orchestras =
-      await prisma.$queryRaw`SELECT * FROM orchestra_type`;
-    const orchestra = orchestraQuery[0];
-
-    return {
-      props: {
-        orchestra,
-        types_orchestras,
-      },
-    };
-  } catch (error) {}
-};
+}
 
 export default withPageAuthRequired(function OrchestraCreateCampaign({
   orchestra,
+  members,
   types_orchestras,
 }: any) {
   const router = useRouter();
-  // const { id } = router.query;
-
+  const { user } = useUser();
   const { id, logo, cover, name, location } = orchestra;
 
   return (
@@ -61,10 +56,17 @@ export default withPageAuthRequired(function OrchestraCreateCampaign({
 
       <StyledMain>
         <aside className="aside-left">
-          <AsideLeft logo={logo} id={id} />
+          <AsideLeft logo={logo} id={id} user={user} />
         </aside>
         <section className="content">
-          <Cover cover={cover} title={name} location={location} />
+          <Cover
+            cover={cover}
+            title={name}
+            location={location}
+            id={id}
+            user={user}
+            members={members}
+          />
           <h2 className="campaign-form-title">Editar Orquesta</h2>
           <OrchestraEditForm
             orchestra={orchestra}
