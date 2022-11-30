aa059e6905fabecb990c805303e7ca8cf6724e54
Jartisick <Jartisick@gmail.com>
2022-11-29 20:40:32 -0400

    changes

--- src/pages/users/[id]/edit.tsx
+++ src/pages/users/[id]/edit.tsx
@@ -1,15 +1,12 @@
 import { useRouter } from "next/router";
 import MainNavBar from "../../../frontend/components/MainNavBar";
-import { Orquestas, Users } from "../../../frontend/utils/fakeDB";
 import Footer from "../../../frontend/components/Footer";
 import AsideRight from "../../../frontend/components/orchestras/AsideRight";
 import { StyledMain } from "../../../frontend/styles/orchestras/sharedStyles";
 import { prisma } from "../../../../lib/prisma";
-import axios from "axios";
 import UserAsideLeft from "../../../frontend/components/users/AsideLeft";
 import UserCover from "../../../frontend/components/users/Cover";
 import EditUser from "../../../frontend/components/users/EditForm";
-import { HOSTNAME } from "../../_app";
 
 export const getServerSideProps = async ({ params }: any) => {
   try {
