9894d725683cc30585568d2db914faf1f24ccc9d
Javier Gonzalez <57018935+hanseyll@users.noreply.github.com>
2022-11-29 20:38:25 -0500

    Update Auth.tsx

--- src/frontend/components/Auth.tsx
+++ src/frontend/components/Auth.tsx
@@ -4,8 +4,7 @@ import { useUser } from "@auth0/nextjs-auth0/dist/frontend/use-user";
 import Cookies from "universal-cookie";
 import { useEffect, useState } from "react";
 import {getUserEmail  } from "../utils/getUserEmail";
-import { toast,ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 
 export default function Auth() {
   const { user, error, isLoading } = useUser();
@@ -33,21 +32,11 @@ export default function Auth() {
   
   } else if(cookie.get('UserloginData')){
     if (!cookie.get("UserloginData").is_active) {
-      toast.error("Verifica e intenta nuevamente.", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
+      alert('usuario baneado ')
       cookie.remove("Userlogin");
       cookie.remove("UserloginData");
       setLoging(cookie.get("Userlogin"));  
       window.location.href='/bannedPage';
-      <ToastContainer />
        
     }else{
       return <LoggedIn handlelogout={handlelogout} />;
