825c59ed7efd7000124c2f3c55cd4918d13f03ce
JuanLange <juancarloslange@gmail.com>
2022-12-01 03:43:32 -0300

    NavBar / mobile & desktop incomplete

--- src/frontend/components/Nav.tsx
+++ src/frontend/components/Nav.tsx
@@ -1,21 +1,48 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Nav(propr: any) {
-  return (
-    <nav>
-      <Link href="/">Inicio</Link>
+	return (
+		<nav>
+			<div className='desktopNav'>
+				<Link href='/'>Inicio</Link>
 
-      <Link href="/orchestras">Orquestas </Link>
+				<Link href='/orchestras'>Orquestas </Link>
 
-      <Link href="/news">Noticias </Link>
+				<Link href='/news'>Noticias </Link>
 
-      <Link href="/events">Eventos </Link>
+				<Link href='/events'>Eventos </Link>
 
-      {/* <Link href="#">Multimedia </Link> */}
+				{/* <Link href="#">Multimedia </Link> */}
 
-      <Link href="/campaigns">Campañas </Link>
+				<Link href='/campaigns'>Campañas </Link>
 
-      {/* <Link href="#">Acerca de </Link> */}
-    </nav>
-  );
+				{/* <Link href="#">Acerca de </Link> */}
+			</div>
+			<div className='mobileNav'>
+				<Link href='/'>
+					<img src='/assets/iconhome.png' />
+				</Link>
+
+				<Link href='/orchestras'>
+					<img src='/assets/iconmusic.png' />
+				</Link>
+
+				<Link href='/news'>
+					<img src='/assets/iconnews.png' />
+				</Link>
+
+				<Link href='/events'>
+					<img src='/assets/iconevents.png' />
+				</Link>
+
+				{/* <Link href="#"><img src='/assets/iconhome.png'/></Link> */}
+
+				<Link href='/campaigns'>
+					<img src='/assets/iconheart.png' />
+				</Link>
+
+				{/* <Link href="#">Acerca de </Link> */}
+			</div>
+		</nav>
+	);
 }
