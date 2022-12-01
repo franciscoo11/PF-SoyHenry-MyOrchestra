1f995014a1a8109e645a0774621401d4932b3856
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-12-01 17:30:05 +0000

    Mensajes de confirmacion con estilo

---
+++ src/pages/confirm.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  background-image: url("bg_01.jpg");
+  max-width: 1440px;
+  margin: 0 auto;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  background-position: center;
+  color: white;
+
+  div {
+    text-align: center;
+    button {
+      background-color: white;
+      border: none;
+      padding: 12px;
+      border-radius: 12px;
+      font-weight: bold;
+
+      :hover {
+        cursor: pointer;
+        background-color: ${({ theme }) => theme.colors.secondary};
+        color: white;
+      }
+    }
+  }
+`;
+
+export default function Confirm() {
+  return (
+    <StyledDiv>
+      <div>
+        <img src="mapa.png" alt="Mapa Orquestas" height="200px" />
+        <br />
+        <img
+          src="isologo.png"
+          alt="Red de Orquestas Populares de Musica Latinoamericana"
+          height="60px"
+        />
+        <h2>
+          GRACIAS POR TU COLABORACIÓN, <br /> EN UNOS INSTANTES SERÁS
+          REDIRIGIDO...
+        </h2>
+        <button>CONTINUAR</button>
+      </div>
+    </StyledDiv>
+  );
+}
--- src/pages/orchestras/[id]/campaigns/[campaign]/mpsuccess.tsx
+++ src/pages/orchestras/[id]/campaigns/[campaign]/mpsuccess.tsx
@@ -4,6 +4,37 @@ import { HOSTNAME } from "../../../../../pages/_app";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 import Cookies from "universal-cookie";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  background-image: url("bg_01.jpg");
+  max-width: 1440px;
+  margin: 0 auto;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  background-position: center;
+  color: white;
+
+  div {
+    text-align: center;
+    button {
+      background-color: white;
+      border: none;
+      padding: 12px;
+      border-radius: 12px;
+      font-weight: bold;
+
+      :hover {
+        cursor: pointer;
+        background-color: ${({ theme }) => theme.colors.secondary};
+        color: white;
+      }
+    }
+  }
+`;
 
 //  interface IPaymentDetail {
 //    paymentDetail: {
@@ -16,11 +47,10 @@ import Cookies from "universal-cookie";
 //    };
 //  }
 
-const mpSuccess = ({ paymentDetail, idOrchestra, idCampaign }: any , ) => {
+const mpSuccess = ({ paymentDetail, idOrchestra, idCampaign }: any) => {
   const router = useRouter();
   const cookie = new Cookies();
   const user = cookie.get("UserloginData");
-
 
   if (!paymentDetail) {
     toast.error(
@@ -41,14 +71,14 @@ const mpSuccess = ({ paymentDetail, idOrchestra, idCampaign }: any , ) => {
   }
 
   const handleButtonRedirect = (e: any) => {
-
-     axios
-      .post('/api/donation', {
+    axios
+      .post("/api/donation", {
         campaignId: idCampaign,
         userId: user.id,
         amount: paymentDetail.mount,
         orchestraId: idOrchestra,
-      }).then(() => {
+      })
+      .then(() => {
         toast.success("Pago realizado", {
           position: "top-center",
           autoClose: 5000,
@@ -59,7 +89,7 @@ const mpSuccess = ({ paymentDetail, idOrchestra, idCampaign }: any , ) => {
           progress: undefined,
           theme: "light",
         });
-        router.push(`/orchestras/${idOrchestra}/campaigns`)
+        router.push(`/orchestras/${idOrchestra}/campaigns`);
       })
       .catch(() => {
         toast.error("No se pudo verificar tu pago.", {
@@ -76,35 +106,41 @@ const mpSuccess = ({ paymentDetail, idOrchestra, idCampaign }: any , ) => {
   };
 
   return (
-    <>
+    <StyledDiv>
       {paymentDetail.status_detail == "accredited" ? (
         <div>
-          <h2>
-            GRACIAS POR TU COLABORACIÓN!!
-          </h2>
+          <img src="mapa.png" alt="Mapa Orquestas" height="200px" />
+          <br />
+          <img
+            src="isologo.png"
+            alt="Red de Orquestas Populares de Musica Latinoamericana"
+            height="60px"
+          />
+          <h2>GRACIAS POR TU COLABORACIÓN!!</h2>
           <button onClick={(e) => handleButtonRedirect(e)}>CONTINUAR</button>
         </div>
       ) : null}
-    </>
+    </StyledDiv>
   );
 };
 
 export async function getServerSideProps(context: any) {
   try {
-    const {id, campaign, payment_id } =  context.query
+    const { id, campaign, payment_id } = context.query;
     const { data: response } = await axios.get(
-      `${HOSTNAME}/api/mercadopago?id=${payment_id}`, {
+      `${HOSTNAME}/api/mercadopago?id=${payment_id}`,
+      {
         headers: {
-          'Accept-Encoding': 'null',
-        }
+          "Accept-Encoding": "null",
+        },
       }
     );
-    
+
     return {
       props: {
         paymentDetail: response,
         idOrchestra: id,
-        idCampaign: campaign
+        idCampaign: campaign,
       },
     };
   } catch (error) {
