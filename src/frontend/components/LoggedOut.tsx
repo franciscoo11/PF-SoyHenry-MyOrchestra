b97707be9046b85b8d0a018d6f7118d2c8663f61
JuanLange <juancarloslange@gmail.com>
2022-12-01 04:53:09 -0300

    mobile / imgs

--- src/frontend/components/LoggedOut.tsx
+++ src/frontend/components/LoggedOut.tsx
@@ -1,11 +1,38 @@
-import Link from "next/link";
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const LogStyle = styled.header`
+	.loginMobile {
+		display: none;
+	}
+
+	.loginMobile img {
+		border: 1px solid lightgray;
+		border-radius: 50%;
+		padding: 10px;
+	}
+
+	@media (max-width: 650px) {
+		.loginDesktop {
+			display: none;
+		}
+		.loginMobile {
+			display: flex;
+		}
+	}
+`;
 
 export default function LoggedOut() {
-  return (
-    <div>
-      <Link href="/api/auth/login" className="log">
-        Iniciar sesión
-      </Link>
-    </div>
-  );
+	return (
+		<LogStyle>
+			<div className='loginDesktop'>
+				<Link href='/api/auth/login'>Iniciar sesión</Link>
+			</div>
+			<div className='loginMobile'>
+				<Link href='/api/auth/login'>
+					<img src='/images/assets/iconuser.png' />
+				</Link>
+			</div>
+		</LogStyle>
+	);
 }
