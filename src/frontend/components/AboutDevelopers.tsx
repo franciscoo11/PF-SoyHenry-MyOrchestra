1d9de5bfda5527e22980f0c675dc477cc7fda4c4
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2022-12-01 08:38:02 +0000

    about dev fixed

--- src/frontend/components/AboutDevelopers.tsx
+++ src/frontend/components/AboutDevelopers.tsx
@@ -6,91 +6,91 @@ import HeroImage from "./HeroImage";
 import { isPropertySignature } from "typescript";
 
 const CardStyle = styled.div`
-	box-sizing: border-box;
-	border: 1px solid ${({ theme }) => theme.colors.lines};
-	display: flex;
-	flex-direction: column;
-	gap: 12px;
-	border-radius: 8px;
+  box-sizing: border-box;
+  border: 1px solid ${({ theme }) => theme.colors.lines};
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  border-radius: 8px;
 
-	.card-header {
-		height: 170px;
-	}
+  .card-header {
+    height: 170px;
+  }
 
-	.card-header img {
-		border-top-left-radius: 8px;
-		border-top-right-radius: 8px;
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
+  .card-header img {
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 
-	.card-content {
-		padding: 0 24px 24px 24px;
-	}
-	.separator {
-		height: 1px;
-		width: 100%;
-		background-color: ${({ theme }) => theme.colors.lines};
-		margin-bottom: 20px;
-	}
-	.orqLocation {
-		font-size: 12px;
-		font-weight: 900;
-		text-transform: uppercase;
-		color: ${({ theme }) => theme.colors.light};
-	}
-	.orqTitle {
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		font-size: 20px;
-		font-weight: 700;
-	}
+  .card-content {
+    padding: 0 24px 24px 24px;
+  }
+  .separator {
+    height: 1px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.lines};
+    margin-bottom: 20px;
+  }
+  .orqLocation {
+    font-size: 12px;
+    font-weight: 900;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.colors.light};
+  }
+  .orqTitle {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 20px;
+    font-weight: 700;
+  }
 
-	.description {
-		font-size: 16px;
-		font-weight: 300;
-		color: ${({ theme }) => theme.colors.light};
-		min-height: 115px;
-	}
+  .description {
+    font-size: 16px;
+    font-weight: 300;
+    color: ${({ theme }) => theme.colors.light};
+    min-height: 115px;
+  }
 
-	.card-counters-container {
-		display: flex;
-		justify-content: space-between;
-		align-items: baseline;
-		margin-bottom: 10px;
-	}
+  .card-counters-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+    margin-bottom: 10px;
+  }
 
-	.card-counter {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+  .card-counter {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 
-	.card-btn-container {
-		.card-btn {
-			background-color: ${({ theme }) => theme.colors.secondary};
-			text-align: center;
-			display: block;
-			width: 100%;
-			padding: 12px;
-			font-size: 1em;
-			color: white;
-			border: none;
-			border-radius: 6px;
+  .card-btn-container {
+    .card-btn {
+      background-color: ${({ theme }) => theme.colors.secondary};
+      text-align: center;
+      display: block;
+      width: 100%;
+      padding: 12px;
+      font-size: 1em;
+      color: white;
+      border: none;
+      border-radius: 6px;
 
-			:hover {
-				filter: brightness(110%);
-				cursor: pointer;
-			}
-		}
-	}
+      :hover {
+        filter: brightness(110%);
+        cursor: pointer;
+      }
+    }
+  }
 `;
 
 interface DeveloperMemberProps {
-  pic?: string;
+  pic: string;
   name: string;
   team: string;
   description: Text;
@@ -98,29 +98,28 @@ interface DeveloperMemberProps {
   LinkUrl: string;
 }
 
-export default function DeveloperMember({ props }: any) {
+export default function DeveloperMember(props: any) {
   return (
     <CardStyle>
       <MainNavBar />
       <div className="card-content">
-      <div className='separator'></div>
-						<div className='card-counters-container'></div>
-            <div className="card-counter">
-              <img src= {props.pic} width="25%" />
-              <div>
-                <div className="card-content">
-                  <div className='orqTitle'>{props.name}</div>
-                   <div className='orqLocation'>{props.team}</div>
-                  <p className='description'>{props.description}</p>
-                  
-                  <div className='card-footer'>
-                </div>
-              </div>
+        <div className="separator"></div>
+        <div className="card-counters-container"></div>
+        <div className="card-counter">
+          <img src={props.pic} width="25%" />
+          <div>
+            <div className="card-content">
+              <div className="orqTitle">{props.name}</div>
+              <div className="orqLocation">{props.team}</div>
+              <p className="description">{props.description}</p>
 
-              <div>{props.GitUrl}</div>
-              <div>{props.LinkUrl}</div>
+              <div className="card-footer"></div>
             </div>
-            </div>
+
+            <div>{props.GitUrl}</div>
+            <div>{props.LinkUrl}</div>
+          </div>
+        </div>
       </div>
     </CardStyle>
   );
--- src/pages/AboutDev.tsx
+++ src/pages/AboutDev.tsx
@@ -1,10 +1,9 @@
-import DeveloperMember  from '../frontend/components/AboutDevelopers';
+import DeveloperMember from "../frontend/components/AboutDevelopers";
 import Head from "next/head";
 import Footer from "../frontend/components/Footer";
 import { HOSTNAME } from "./_app";
-import HomeCards from '../frontend/components/HomeCards';
+import HomeCards from "../frontend/components/HomeCards";
 import { Developers } from "../frontend/utils/fakeDB";
-
 
 export default function AboutDev(props: any) {
   return (
@@ -12,22 +11,19 @@ export default function AboutDev(props: any) {
       <Head>
         <title>Red de Orquestas Populares de Música Latinoamericana</title>
       </Head>
-      { Developers?.map((e)=> {
-        return(
-          <DeveloperMember 
-          pic= {e.pic}
-          name= {e.name}
-          team= {e.team}
-          description= {e.description}
-          GitUrl= {e.GitUrl}
-          LinkUrl= {e.LinkUrl}
+      {Developers.map((e) => {
+        return (
+          <DeveloperMember
+            pic={e.pic}
+            name={e.name}
+            team={e.team}
+            description={e.description}
+            GitUrl={e.GitUrl}
+            LinkUrl={e.LinkUrl}
           />
-        )
-      }) }
+        );
+      })}
       <Footer />
     </>
   );
 }
-
-
-
